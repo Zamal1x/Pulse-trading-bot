7bc65f72c73c7466d275dbe14717c6cd6d3fbe53
MD ZAMAL AHMED <113609020+Zamal1x@users.noreply.github.com>
2026-06-28 09:15:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from telegram.ext import Application
+from bot import register
+from config import BOT_TOKEN
 
+app = Application.builder().token(BOT_TOKEN).build()
+
+register(app)
+
+app.run_polling()
